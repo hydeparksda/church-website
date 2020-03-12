aa367356ca0580015fea9ff5ea9eaff8bf40f0c7
Derek Scarlett <dscarlett@wayfair.com>
2020-03-11 23:17:46 -0400

    Added AboutUs page , our pastor component

--- hyde-park/src/App.js
+++ hyde-park/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Home from './components/Home/Home';
+import AboutUs from './components/AboutUs/AboutUs';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      {/* <Home /> */}
+      <AboutUs />
     </div>
   );
 }
